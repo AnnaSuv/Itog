49eae76c4557da238699e07c5e56b89e94525ead
Anna Suv <multguga@gmail.com>
2024-05-15 19:13:47 +0300

    README

--- ITOG/obj/Debug/net8.0/ITOG.AssemblyInfo.cs
+++ ITOG/obj/Debug/net8.0/ITOG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITOG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9972c41d0c71dcb571eb7180b811922e287c9f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITOG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITOG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
